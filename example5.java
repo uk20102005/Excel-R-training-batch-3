39692507cda71c4d1fb2338a30c9ec8f33567cb0
uk20102005 <udaykiranerragoud@gmail.com>
2025-01-03 10:53:26 +0530

    Update example5.java

--- example5.java
+++ example5.java
@@ -3,16 +3,11 @@ class example5{
                 String userid="prasunamba";
                 String password="prasunamba@001";
 
-        if(userid=="prasunamba" && password=="prasunamba@001"){
+         while(userid=="prasunamba" && password=="prasunamba@001"){
 
             System.out.println("HELLO PRASUNAMBA WELLCOME TO MALLAREDDY UNIVERSITY ");
+break;
 
-        }
-
-        else{
-
-            System.out.println("INVAILD USERID OR PASSWORD");
-
-        }
+}
     } 
 }
