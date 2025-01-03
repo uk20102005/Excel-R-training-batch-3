c8b3dd90ff6ad3e1a7af0c1f7bef58f214905022
uk20102005 <udaykiranerragoud@gmail.com>
2025-01-03 10:47:45 +0530

    Add files via upload

---
+++ example5.java
@@ -0,0 +1,18 @@
+class example5{
+        public static void main(String[] args){
+                String userid="prasunamba";
+                String password="prasunamba@001";
+
+        if(userid=="prasunamba" && password=="prasunamba@001"){
+
+            System.out.println("HAPPY NEW YEAR 2025");
+
+        }
+
+        else{
+
+            System.out.println("INVAILD USERID OR PASSWORD");
+
+        }
+    } 
+}
