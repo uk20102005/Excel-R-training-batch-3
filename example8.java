fe1357f5821b4fcb85660680c0db19093eb8949b
uk20102005 <udaykiranerragoud@gmail.com>
2025-01-04 10:09:50 +0530

    Add files via upload

---
+++ example8.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class example8{
+   public void accpet_details(){
+    String Customer_name = "sathi";
+    long Account_number = "123456789";
+     Double Balance = 10;
+     Scanner scan =new Scanner(System.in);
+     System.out.println("Customer Name: ");
+     customer_name =scan.nextLine();
+     System.out.println("Account Number: ");
+     Account_number = scan.nextLong();
+   }
+   void Display_details(){
+    System.out.println("Customer Name: "+customer_name);
+    System.out.println("your balance is:"+Balance);
+   }
+
+public static void main (String[] args){
+    example8 obj = new example8();
+    obj.accpet_details();
+    obj.Display_details();
+
+}
+}
