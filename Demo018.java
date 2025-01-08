bb8f025ff838ae0da70ce753da06ac4788ddcf46
uk20102005 <udaykiranerragoud@gmail.com>
2025-01-08 11:32:11 +0530

    Add files via upload

---
+++ Demo018.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Demo018{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of minutes: ");
+        long minutes = sc.nextLong();
+        long years = minutes / (60 * 24 * 365);
+        long remainingDays = (minutes % (60 * 24 * 365)) / (60 * 24);
+        System.out.println("Years: "+years+" Days: "+remainingDays);
+    }
+}
