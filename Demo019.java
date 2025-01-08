b51bfc5a4f9f62218cf39dc6deef506a47c0c475
uk20102005 <udaykiranerragoud@gmail.com>
2025-01-08 11:36:31 +0530

    Add files via upload

---
+++ Demo019.java
@@ -0,0 +1,13 @@
+public class Demo019 {
+    public static void main(String[] args) {
+        int number = 42; // Example number
+
+        int roundedNumber = roundToNearestFive(number);
+
+        System.out.println("The number " + number + " rounded to the nearest 5 is " + roundedNumber + ".");
+    }
+
+    public static int roundToNearestFive(int num) {
+        return (int) (Math.round(num / 5.0) * 5);
+    }
+}
