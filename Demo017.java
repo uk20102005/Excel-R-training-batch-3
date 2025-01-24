02bf76b4edc5e32ddd100e7a22b433a0cd02f291
uk20102005 <udaykiranerragoud@gmail.com>
2025-01-24 12:35:29 +0530

    sum of digits

--- Demo017.java
+++ Demo017.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Demo017 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
 
