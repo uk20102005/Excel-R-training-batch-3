8c626e0684b7e03b58172e8dff5fed76e09dbd6c
uk20102005 <udaykiranerragoud@gmail.com>
2025-01-24 12:28:53 +0530

    taking username ,age and phone number from user

--- Demo007.java
+++ Demo007.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class example6{
+public class Demo007{
     public static void main (String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter your name: ");
