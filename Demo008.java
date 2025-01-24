e2ee6c6137a35544f051a05551338442f0d5bf44
uk20102005 <udaykiranerragoud@gmail.com>
2025-01-24 12:30:05 +0530

    username and password

--- Demo008.java
+++ Demo008.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class example7{
+public class Demo008{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String username,pass;
