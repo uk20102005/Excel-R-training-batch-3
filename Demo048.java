d553cd8de9cf34cfced9f21254fcc982407b3640
uk20102005 <udaykiranerragoud@gmail.com>
2025-01-25 12:28:18 +0530

    SumOfDigits

---
+++ Demo048.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Demo048{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+
+        int sum = sumOfDigits(num);
+        System.out.println("Sum of digits: " + sum);
+    }
+
+    public static int sumOfDigits(int num) {
+        int sum = 0;
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+}
