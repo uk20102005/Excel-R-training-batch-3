fb5e4ff8fb5d71433ee6f21f34b0220387048d31
uk20102005 <udaykiranerragoud@gmail.com>
2025-01-31 12:39:03 +0530

    to print 5 table.

---
+++ Demo050.java
@@ -0,0 +1,8 @@
+public class Demo050 {
+    public static void main(String[] args) {
+        System.out.println("5 Times Table:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("5 x " + i + " = " + (5 * i));
+        }
+    }
+}
