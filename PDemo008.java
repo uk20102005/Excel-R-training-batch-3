7cd6aa3afec9d71b1ca0e3c0f3532ab0d776108f
uday <udaykiranerragoud@gmail.com>
2025-02-01 12:50:32 +0530

    “”

---
+++ PDemo008.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class PDemo008{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String username,pass;
+        do{
+            System.out.println("Hello Mallareddy Unversity, Please Login: ");
+            System.out.print("Enter username: ");
+            username = sc.next();
+            System.out.print("Enter password: ");
+            pass = sc.next();
+        }while((!username.equals("uday"))||(!pass.equals("uday@001")));
+        System.out.println("Successfully logged in.");
+    }
+}
