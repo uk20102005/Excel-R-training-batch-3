acddefd088947d56198a57b01f313b5d52cc862d
uk20102005 <udaykiranerragoud@gmail.com>
2025-02-11 07:07:00 +0000

    Sorting an array without using predefined methods

---
+++ Demo074.java
@@ -0,0 +1,27 @@
+public class Demo074 {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Sorted array: ");
+        bubbleSort(array);
+        printArray(array);
+    }
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (array[j] > array[j+1]) {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
